8dd2c24bba5f48948df4b26fa4f05b59365a2638
Michelle Peralbo <michelle.peralvo@thoughtworks.com>
2019-11-15 09:43:09 -0500

    add connection to mongo logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Item = require('../models/Item')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  //const Item = require('./models/Item');
+  Item.find()
+  .then(items => res.render('index', { items }))
+  .catch(err => res.status(404).json({ msg: 'No items found' }));
+
+
 });
 
 module.exports = router;
